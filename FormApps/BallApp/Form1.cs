8f27e5ac37a2e1781e36949249a4e773adbccf3f
NegishiYu <oibc35141@ojs.ac.jp>
2024-05-08 14:43:37 +0900

    テニスボール作成

--- FormApps/BallApp/Form1.cs
+++ FormApps/BallApp/Form1.cs
@@ -1,5 +1,6 @@
 namespace BallApp {
     public partial class Form1 : Form {
+        TennisBall tennisBall;
         SoccerBall soccerBall;
         PictureBox pb;
 
@@ -11,28 +12,50 @@ namespace BallApp {
 
         //フォームが最初にロードされるとき一度だけ実行される
         private void Form1_Load(object sender, EventArgs e) {
-            
+
         }
 
         private void timer1_Tick(object sender, EventArgs e) {
-
             soccerBall.Move();
+
             pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);
+        }
+        private void timer2_Tick(object sender, EventArgs e) {
+            tennisBall.Move();
+
+            pb.Location = new Point((int)tennisBall.PosX, (int)tennisBall.PosY);
         }
 
         private void Form1_MouseClick(object sender, MouseEventArgs e) {
 
-             pb = new PictureBox();   //画像を表示するコントロール
+            pb = new PictureBox();   //画像を表示するコントロール
             pb.Size = new Size(50, 50);
 
-;           soccerBall = new SoccerBall(e.X,e.Y);
+            if (e.Button == MouseButtons.Left) {
 
-            pb.Image = soccerBall.Image;
-            pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);
-            pb.SizeMode = PictureBoxSizeMode.StretchImage;
-            pb.Parent = this;
+                soccerBall = new SoccerBall(e.X - 25, e.Y - 25);
 
-            timer1.Start();
+                pb.Image = soccerBall.Image;
+                pb.Location = new Point((int)soccerBall.PosX, (int)soccerBall.PosY);
+                pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                pb.Parent = this;
+
+                timer1.Start();
+
+            } else if (e.Button == MouseButtons.Right) {
+
+                tennisBall = new TennisBall(e.X, e.Y);
+
+                pb.Image = tennisBall.Image;
+                pb.Location = new Point((int)tennisBall.PosX, (int)tennisBall.PosY);
+                pb.SizeMode = PictureBoxSizeMode.StretchImage;
+                pb.Parent = this;
+
+                
+            }
+
+
+
 
         }
     }
--- FormApps/BallApp/TennisBall.cs
+++ FormApps/BallApp/TennisBall.cs
@@ -2,26 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace BallApp {
-    internal class SoccerBall :Obj{
-        public SoccerBall(double xp, double yp)
-            : base(xp, yp, @"Picture\soccer_ball.png") {
+    internal class TennisBall : Obj {
+
+        public TennisBall(double xp, double yp)
+            : base(xp, yp, @"Picture\tennis_ball.png") {
 
             MoveX = 10; //移動量設定
             MoveY = 10;
         }
 
         public override bool Move() {
+            
 
-
-            if (PosX > 750 || PosX < 0) {
+            if(PosX > 750 || PosX < 0) {
                 MoveX = -MoveX;
             }
 
-            if (PosY > 500 || PosY < 0) {
+            if(PosY > 500 || PosY < 0) {
                 MoveY = -MoveY;
             }
             PosX += MoveX;
@@ -30,4 +30,3 @@ namespace BallApp {
         }
     }
 }
-
