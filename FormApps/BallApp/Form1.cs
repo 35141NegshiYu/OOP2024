b4f102b7cff17a53b7da4fab0e684f556ac20d74
NegishiYu <oibc35141@ojs.ac.jp>
2024-05-15 11:49:17 +0900

    ラムダ式

--- FormApps/BallApp/Form1.cs
+++ FormApps/BallApp/Form1.cs
@@ -39,7 +39,7 @@ namespace BallApp {
         private void timer1_Tick(object sender, EventArgs e) {
 
             for (int i = 0; i < balls.Count; i++) {
-                balls[i].Move(pbBar, pbs[i]);
+                balls[i].Move(pdBar, pbs[i]);
                 pbs[i].Location = new Point((int)balls[i].PosX, (int)balls[i].PosY);
 
             }
