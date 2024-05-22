9ec888f7b492e66e6930fda7a042f85b415e74b1
NegishiYu <oibc35141@ojs.ac.jp>
2024-05-22 12:06:15 +0900

    3-2終了

--- 演習問題/Exercise02/Program.cs
+++ 演習問題/Exercise02/Program.cs
@@ -15,15 +15,15 @@ namespace Exercise02 {
             Exercise2_1(names);
             Console.WriteLine();
 
-            Console.WriteLine("***** 3.2.1 *****");
+            Console.WriteLine("***** 3.2.2 *****");
             Exercise2_2(names);
             Console.WriteLine();
 
-            Console.WriteLine("***** 3.2.1 *****");
+            Console.WriteLine("***** 3.2.3 *****");
             Exercise2_3(names);
             Console.WriteLine();
 
-            Console.WriteLine("***** 3.2.1 *****");
+            Console.WriteLine("***** 3.2.4 *****");
             Exercise2_4(names);
             Console.WriteLine();
         }
@@ -45,16 +45,22 @@ namespace Exercise02 {
         }
 
         private static void Exercise2_2(List<string> names) {
-            Console.WriteLine("都市名を入力。空行で終了");
-            var count = 
+            var count = names.Count(s => s.Contains('o'));
+            Console.WriteLine(count);
         }
 
         private static void Exercise2_3(List<string> names) {
-            Console.WriteLine("都市名を入力。空行で終了");
+            var array = names.Where(s => s.Contains("o")).ToArray();
+            foreach(var item in array) {
+                Console.WriteLine(item);
+            }
         }
 
         private static void Exercise2_4(List<string> names) {
-            Console.WriteLine("都市名を入力。空行で終了");
+            var selected = names.Where(s => s.StartsWith("B")).Select(s => new { s.Length,s });
+            foreach(var obj in selected) {
+                Console.WriteLine(obj.s + "," + obj.Length);
+            }
         }
     }
 }
