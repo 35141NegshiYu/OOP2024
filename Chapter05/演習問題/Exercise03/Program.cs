14899c6cfc313cad64aabb72994a499f570509b0
NegishiYu <oibc35141@ojs.ac.jp>
2024-05-30 15:06:15 +0900

    問題演習5.3.4

--- Chapter05/演習問題/Exercise03/Program.cs
+++ Chapter05/演習問題/Exercise03/Program.cs
@@ -47,7 +47,10 @@ namespace Exercise03 {
     
 
         private static void Exercise3_4(string text) {
-            
+            var words = text.Split(' ').Where(s=>s.Length > 4);
+            foreach(var word in words) {
+                Console.WriteLine(word);
+            }
         }
 
         private static void Exercise3_5(string text) {
