8c07e85a7cd2d24db4933d3326a23c36a36dea13
NegishiYu <oibc35141@ojs.ac.jp>
2024-06-05 13:11:48 +0900

    第６章演習問題開始

---
+++ Chapter06/Section01/Program.cs
@@ -0,0 +1,32 @@
+﻿using Chapter06;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            /*var pages = Books.GetBooks().Max(x => x.Pages);
+            var bookTitle = Books.GetBooks().FirstOrDefault(b => b.Pages == pages);
+
+            Console.WriteLine(bookTitle.Title);
+
+            var title = Books.GetBooks().Max(x=>x.Title);
+            Console.WriteLine(title);
+            */
+
+            var books = Books.GetBooks().OrderByDescending(x => x.Price).ToList();
+
+            //books.ForEach(b=>Console.WriteLine(b.Title + ":" + b.Price));
+
+            foreach (var book in books) {
+                Console.WriteLine(book.Title + ":" + book.Price);
+            }
+            
+
+        }
+    }
+}
