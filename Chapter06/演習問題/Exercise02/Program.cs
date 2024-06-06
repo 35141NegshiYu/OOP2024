72c707a188ef3cdf410862518f007f8482dffe86
NegishiYu <oibc35141@ojs.ac.jp>
2024-06-06 13:41:07 +0900

    7完成

--- Chapter06/演習問題/Exercise02/Program.cs
+++ Chapter06/演習問題/Exercise02/Program.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Exercise02 {
-    internal class Program {
+    internal class Book {
+        public string Title { get; private set; }
+        public int Price { get; private set; }
+        public int Pages { get; private set; }
+
         static void Main(string[] args) {
             var books = new List<Book> {
                new Book { Title = "C#プログラミングの新常識", Price = 3800, Pages = 378 },
@@ -41,31 +46,46 @@ namespace Exercise02 {
         }
 
         private static void Exercise2_1(List<Book> books) {
-            throw new NotImplementedException();
+            var result = books.Where(n => n.Title == "ワンダフル・C#ライフ");
+            foreach (var book in result)
+                Console.WriteLine("{0}:{1}円{2}ページ ", book.Title, book.Price, book.Pages);
+
         }
 
         private static void Exercise2_2(List<Book> books) {
-            throw new NotImplementedException();
+            int count = books.Count(b => b.Title.Contains("C#"));
+            Console.WriteLine(count);
         }
 
         private static void Exercise2_3(List<Book> books) {
-            throw new NotImplementedException();
+            var average = books.Where(b => b.Title.Contains("C#")).Average(b => b.Pages);
+            Console.WriteLine(average);
         }
 
         private static void Exercise2_4(List<Book> books) {
-            throw new NotImplementedException();
+            var book = books.FirstOrDefault(b => b.Price >= 4000);
+            if (book != null)
+                Console.WriteLine(book.Title);
         }
 
         private static void Exercise2_5(List<Book> books) {
-            throw new NotImplementedException();
+            var pages = books.Where(b => b.Price < 4000).Max(b => b.Pages);
+            Console.WriteLine(pages);
         }
 
         private static void Exercise2_6(List<Book> books) {
-            throw new NotImplementedException();
+            var pages = books.Where(p => p.Pages > 400).OrderByDescending(b => b.Price);
+            foreach (var book in pages) {
+                Console.WriteLine("{0}:{1}円", book.Title, book.Price);
+            }
         }
 
         private static void Exercise2_7(List<Book> books) {
-            throw new NotImplementedException();
+            var selected = books.Where(b => b.Title.Contains("C#") && b.Pages <= 500);
+            foreach (var book in selected) {
+                Console.WriteLine(book.Title);
+                
+            }
         }
     }
 }
