c6071c2f692d3f789a5379ce31f31baf16935aef
NegishiYu <oibc35141@ojs.ac.jp>
2024-06-18 11:04:43 +0900

    設問１_Student完成

---
+++ TestProject/Test01/Student.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Test01 {
+    class Student {
+        //名前
+        public string Name { get; set; }
+        //科目名
+        public string Subject { get; set; }
+        //点数
+        public int Score { get; set; }
+
+
+
+    }
+}
