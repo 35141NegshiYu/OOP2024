435f504f3d8d7de1701f63f17a2a49c98a87d9d9
NegishiYu <oibc35141@ojs.ac.jp>
2024-09-03 12:06:51 +0900

    タイトル終了

---
+++ FormApps/FormApp/RssReader/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace RssReader {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btGet_Click(object sender, EventArgs e) {
+            using (var wc = new WebClient()) {
+                var url = wc.OpenRead(tbRssUrl.Text);
+                var xdoc = XDocument.Load(url);
+                var xtitles = xdoc.Root.Descendants("item")
+                                    .Select(item => new {
+                                        title = (string)item.Element("title"),
+                                        link = (string)item.Element("link")
+                                    });
+
+
+                foreach (var x in xtitles) {
+                    
+                    lbRssTitle.Items.Add(x);
+
+                    
+                }
+                    
+            }
+        }
+    }
+}
