3a95fdd0bc6380aebc6617789e1fbe30a2b05b95
NegishiYu <oibc35141@ojs.ac.jp>
2024-09-30 15:02:42 +0900

    Exercise1_4おわり

---
+++ Chapter15/Section01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var years = new int[] { 2013, 2016 };
+
+            var groups = Library.Books
+                .GroupBy(b => b.PublishedYear)
+                .OrderBy(g => g.Key);
+
+
+            foreach(var g in groups ) {
+                Console.WriteLine($"{ g.Key} 年");
+                foreach(var book in g) {
+                    Console.WriteLine($"     {book}");
+                }
+            }
+
+            
+        }
+    }
+}
---
+++ Chapter15/演習問題/Exercise01/Program.cs
@@ -0,0 +1,71 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Exercise1_2();
+            Console.WriteLine();
+            Exercise1_3();
+            Console.WriteLine();
+            Exercise1_4();
+            Console.WriteLine();
+            Exercise1_5();
+            Console.WriteLine();
+            Exercise1_6();
+            Console.WriteLine();
+            Exercise1_7();
+            Console.WriteLine();
+            Exercise1_8();
+
+            Console.ReadLine();
+        }
+
+        private static void Exercise1_2() {
+            var maxPrice = Library.Books.Max(b => b.Price);
+            Console.WriteLine(maxPrice);
+        }
+
+        private static void Exercise1_3() {
+            var counts = Library.Books
+                .GroupBy(b => b.PublishedYear)
+                .Select(g => new {
+                    year = g.Key,
+                    count = g.Count()})
+                .OrderBy(g => g.year);
+
+            Console.WriteLine("書籍のカウント (発行年ごと):");
+            foreach (var count in counts) {
+                Console.WriteLine($"{count.year}年: {count.count} 冊");
+            }
+        }
+
+        private static void Exercise1_4() {
+            var sortedBooks = Library.Books
+            .OrderByDescending(b => b.PublishedYear)  // 発行年で降順にソート
+            .ThenByDescending(b => b.Price)              // 価格で降順にソート
+            .ToList();
+
+            Console.WriteLine("書籍のリスト (発行年、価格の順):");
+            foreach (var book in sortedBooks) {
+                Console.WriteLine($"Title: {book.Title},Price: {book.Price:C}, Year: {book.PublishedYear}");
+            }
+        }
+
+        private static void Exercise1_5() {
+        }
+
+        private static void Exercise1_6() {
+        }
+
+        private static void Exercise1_7() {
+        }
+
+        private static void Exercise1_8() {
+        }
+    }
+}
