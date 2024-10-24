97d5d38c46e62c8fd960f36f996a2f472c18259f
根岸　優羽 <oibc35141@ojs.ac.jp>
2024-10-24 11:03:44 +0900

    スライダーも一緒に変更まで完成

--- WPF/CollorChecker/MainWindow.xaml.cs
+++ WPF/CollorChecker/MainWindow.xaml.cs
@@ -19,11 +19,20 @@ namespace CollorChecker {
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
+        MyColor currentColor = new MyColor();   //現在設定してる色情報
         public MainWindow() {
             InitializeComponent();
         }
 
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) {
+            currentColor = new MyColor {
+
+                Color = Color.FromRgb((byte)rSlider.Value, (byte)gSlider.Value, (byte)bSlider.Value),
+                Name = ""
+            };
+        
+            colorArea.Background = new SolidColorBrush(currentColor.Color);
+
             int rvalue = (int)rSlider.Value;
             int gvalue = (int)gSlider.Value;
             int bvalue = (int)bSlider.Value;
@@ -32,7 +41,34 @@ namespace CollorChecker {
             gValue.Text = gvalue.ToString();
             bValue.Text = bvalue.ToString();
 
-            colorArea.Background = new SolidColorBrush(Color.FromRgb((byte)rvalue, (byte)gvalue, (byte)bvalue));
+           
+        
+        }
+
+        private void stockButton_Click(object sender, RoutedEventArgs e) {
+            stockList.Items.Insert(0, currentColor);
+        }
+    
+
+        private void stockList_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            if (stockList.SelectedItem is MyColor selectedColor) {
+                // 選択されたMyColorから色を取得
+                colorArea.Background = new SolidColorBrush(selectedColor.Color);
+
+                //スライダーの更新
+                rSlider.Value = selectedColor.Color.R;
+                gSlider.Value = selectedColor.Color.G;
+                bSlider.Value = selectedColor.Color.B;
+
+                //テキストボックスの値も更新
+                rValue.Text = selectedColor.Color.R.ToString();
+                gValue.Text = selectedColor.Color.G.ToString();
+                bValue.Text = selectedColor.Color.B.ToString();
+            }
+        }
+
+        private void colorSelectComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+
         }
     }
 }
