eb76445068a7e1d25a3224ee540579325be1e803
根岸　優羽 <oibc35141@ojs.ac.jp>
2024-11-06 14:44:34 +0900

    Viewに移した

---
+++ WPF/SampleUnitConverter/Model/DistanceUnit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace SampleUnitConverter {
+    public class DistanceUnit {
+        public string Name { get; set; }
+        public int Coefficient { get; set; }
+        public override string ToString() {
+            return this.Name;
+        }
+
+    }
+}
