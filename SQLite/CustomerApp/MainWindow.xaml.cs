bb20b20a0b2c4e44e6f289fdc55723ee31ad779f
根岸　優羽 <oibc35141@ojs.ac.jp>
2024-11-18 14:00:11 +0900

    Update完成

--- SQLite/CustomerApp/MainWindow.xaml.cs
+++ SQLite/CustomerApp/MainWindow.xaml.cs
@@ -3,29 +3,19 @@ using SQLite;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CustomerApp {
-    /// <summary>
-    /// MainWindow.xaml の相互作用ロジック
-    /// </summary>
     public partial class MainWindow : Window {
         List<Customer> _customers;
+
         public MainWindow() {
             InitializeComponent();
-            ReadDatabase();
+            ReadDatabase();  // データベースを読み込む
         }
 
+        // Saveボタンをクリックしたとき
         private void SaveButton_Click(object sender, RoutedEventArgs e) {
             var customer = new Customer() {
                 Name = NameTextBox.Text,
@@ -33,34 +23,75 @@ namespace CustomerApp {
                 Address = AddressTextBox.Text,
             };
 
-            using(var connection = new SQLiteConnection(App.databasePass)) {
+            using (var connection = new SQLiteConnection(App.databasePass)) {
                 connection.CreateTable<Customer>();
-                connection.Insert(customer);
+                connection.Insert(customer);  // 顧客データを挿入
             }
-            ReadDatabase();//ListView表示
+            ReadDatabase();  // 更新後にデータを再読み込み
         }
-        
 
-        private void ReadButton_Click(object sender, RoutedEventArgs e) {
-            //ReadDatabase();
+        // ListViewの選択された顧客情報をテキストボックスに表示
+        private void CustomerListView_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            var selectedCustomer = CustomerListView.SelectedItem as Customer;
+            if (selectedCustomer != null) {
+                NameTextBox.Text = selectedCustomer.Name;
+                PhoneTextBox.Text = selectedCustomer.Phone;
+                AddressTextBox.Text = selectedCustomer.Address;
+            }
         }
-        //ListView表示
+
+        // 顧客情報の更新
+        private void UpdateButton_Click(object sender, RoutedEventArgs e) {
+            var selectedCustomer = CustomerListView.SelectedItem as Customer;
+            if (selectedCustomer == null) {
+                MessageBox.Show("更新する顧客を選択してください");
+                return;
+            }
+
+            bool isUpdated = false;
+
+            // 名前が変更された場合
+            if (selectedCustomer.Name != NameTextBox.Text) {
+                selectedCustomer.Name = NameTextBox.Text;
+                isUpdated = true;
+            }
+
+            // 電話番号が変更された場合
+            if (selectedCustomer.Phone != PhoneTextBox.Text) {
+                selectedCustomer.Phone = PhoneTextBox.Text;
+                isUpdated = true;
+            }
+
+            // 住所が変更された場合
+            if (selectedCustomer.Address != AddressTextBox.Text) {
+                selectedCustomer.Address = AddressTextBox.Text;
+                isUpdated = true;
+            }
+
+            if (isUpdated) {
+                using (var connection = new SQLiteConnection(App.databasePass)) {
+                    connection.CreateTable<Customer>();
+                    connection.Update(selectedCustomer);  // 顧客情報をデータベースで更新
+                }
+
+                // 更新後、ListViewを再度読み込み
+                ReadDatabase();
+                MessageBox.Show("顧客情報が更新されました。");
+            } else {
+                MessageBox.Show("変更がありません。");
+            }
+        }
+
+        // 顧客データを読み込んでListViewに表示
         private void ReadDatabase() {
             using (var connection = new SQLiteConnection(App.databasePass)) {
                 connection.CreateTable<Customer>();
                 _customers = connection.Table<Customer>().ToList();
-
-                CustomerListView.ItemsSource = _customers;
-
+                CustomerListView.ItemsSource = _customers;  // ListViewを更新
             }
         }
 
-        //数値が変わったらのメソッド
-        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e) {
-            var filterList = _customers.Where(x=>x.Name.Contains(SearchTextBox.Text)).ToList();
-            CustomerListView.ItemsSource = filterList;
-        }
-
+        // 顧客を削除する
         private void DeleteButton_Click(object sender, RoutedEventArgs e) {
             var item = CustomerListView.SelectedItem as Customer;
             if (item == null) {
@@ -69,11 +100,15 @@ namespace CustomerApp {
             }
             using (var connection = new SQLiteConnection(App.databasePass)) {
                 connection.CreateTable<Customer>();
-                connection.Delete(item);
-
-                ReadDatabase();
-
+                connection.Delete(item);  // 顧客データを削除
+                ReadDatabase();  // 更新後にデータを再読み込み
             }
+        }
+
+        // 検索ボックスが変更されたとき
+        private void SearchTextBox_TextChanged(object sender, TextChangedEventArgs e) {
+            var filterList = _customers.Where(x => x.Name.Contains(SearchTextBox.Text)).ToList();
+            CustomerListView.ItemsSource = filterList;  // 検索に応じたフィルタリング
         }
     }
 }
